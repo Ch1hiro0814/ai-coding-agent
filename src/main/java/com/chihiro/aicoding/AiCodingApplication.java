69b7afa9234120f91284412b6ac6a3ab72a7aa57
Chihiro <chihirowzk@gmail.com>
2025-08-19 23:37:34 -0400

    二、用户模块后端
    1.开发Mybatis Flex代码生成器
    2.开发核心功能：用户注册、登录、获取登录用户信息、用户注销等
    3.全局权限校验 AOP
    4.开发管理员增删改查接口
    5.自定义配置解决丢失精度问题

--- src/main/java/com/chihiro/aicoding/AiCodingApplication.java
+++ src/main/java/com/chihiro/aicoding/AiCodingApplication.java
@@ -1,9 +1,11 @@
 package com.chihiro.aicoding;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.chihiro.aicoding.mapper")
 public class AiCodingApplication {
 
     public static void main(String[] args) {
